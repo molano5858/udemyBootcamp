7cb29989e99b60604a9c4833daa3ab252c9ef775
Felipe Molano <molano5858@gmail.com>
2023-03-14 17:55:15 -0500

    Creacion primer capitulo

---
+++ git.js
@@ -0,0 +1,12 @@
+// git init
+// git add .
+// ls -a muestra lista archivos incluso los ocultos
+// git commit -m'mensaje'
+// git log
+// git status
+// git diff muestra la diferencia que hay entre el ultimo commit y el archivo actual
+// git diff 'nombre del archivo especifico' muestra la diferencia del archivo especifico
+// git checkout ejemploGit3.js : devuelve al ultimo commit el archivo especificado
+// git merge myBranch : nombre de la rama que queremos fusionar
+// git remote add origin 'url'
+// git push origin master
